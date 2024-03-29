ba9eb85a96ac73d736619adc86c286bd520896bb
fkamouacs <kamouaferid@gmail.com>
2024-03-29 19:03:25 -0400

    deploy

--- backend/server.js
+++ backend/server.js
@@ -45,9 +45,9 @@ app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 } )
 
-app.listen(443, function(err){
+app.listen(80 , function(err){
     if (err) console.log("Error in server setup")
-    console.log("Server listening on Port", 443 );
+    console.log("Server listening on Port", 80);
 })
 
 app.get("/", (req, res) => {
--- frontend/src/components/login_page.js
+++ frontend/src/components/login_page.js
@@ -10,7 +10,7 @@ import loginImagePath from '../loginPicture.jpg';
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: `http://localhost:443`,
+    baseURL: `http://localhost:80`,
 });
 
 export default function LoginPage(){
