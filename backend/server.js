eaadf8ae3fbf3fd4a7e151d72868754de1b8cc4f
fkamouacs <kamouaferid@gmail.com>
2024-03-29 18:04:40 -0400

    deploy

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT || 3001, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
