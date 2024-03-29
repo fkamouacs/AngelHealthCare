d57608077962e74f3660bbf971113ecd1719fcc1
Impeg <huifu.li@stonybrook.edu>
2024-03-29 15:52:09 -0400

    ymlk

--- backend/server.test.js
+++ backend/server.test.js
@@ -1,5 +1,21 @@
-// const sum = require('./server');
+const request = require('supertest');
+const server = require('./server');
 
-// test('adds 1 + 2 to equal 3', () => {
-//   expect(3).toBe(3);
-// });
+describe('Test Express Routes', () => {
+    test('GET /', async () => {
+        const response = await request(server)
+            .get('/')
+            .expect('Content-Type', /json/)
+            .expect(200);
+        expect(response.body.msg).toBe("The get('/') was successful!");
+    });
+
+    test('PUT /', async () => {
+        const response = await request(server)
+            .put('/')
+            .expect('Content-Type', /json/)
+            .expect(200);
+        expect(response.body.msg).toBe("The put('/') was successful!");
+    });
+
+});
