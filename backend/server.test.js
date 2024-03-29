c5a81a2a8491aaa8aee4acff4b8a4031a6491df0
Impeg <huifu.li@stonybrook.edu>
2024-03-29 16:29:09 -0400

    cypress

--- backend/server.test.js
+++ backend/server.test.js
@@ -2,18 +2,18 @@ const request = require('supertest');
 const server = require('./server');
 
 describe('Test Express Routes', () => {
-    test('GET /', async () => {
-        const response = await request(server)
+    test('GET /', () => {
+        const response = request(server)
             .get('/')
-            .expect('Content-Type', /json/)
+            // .expect('Content-Type', /json/)
             .expect(200);
         expect(response.body.msg).toBe("The get('/') was successful!");
     });
 
-    test('PUT /', async () => {
-        const response = await request(server)
+    test('PUT /', () => {
+        const response = request(server)
             .put('/')
-            .expect('Content-Type', /json/)
+            // .expect('Content-Type', /json/)
             .expect(200);
         expect(response.body.msg).toBe("The put('/') was successful!");
     });
