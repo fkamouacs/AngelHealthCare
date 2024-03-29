af79c7f4d90696d7c76fca69695d2a443f3ea4b2
Impeg <huifu.li@stonybrook.edu>
2024-03-29 16:24:55 -0400

    cypress

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ app.use(
 app.use(express.json());
 
 __dirname = path.resolve();
-app.use(expres.static(path.join(__dirname, '/frontend/build')))
+app.use(express.static(path.join(__dirname, '/frontend/build')))
 
 app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
