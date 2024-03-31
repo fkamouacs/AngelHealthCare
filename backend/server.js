b1aabc8b30d9e82ef42f0c04c6c1b36ba7ca9d39
fkamouacs <kamouaferid@gmail.com>
2024-03-31 09:55:35 -0400

    deploy

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ server.listen(process.env.PORT || 3000, () => {
 
 // needed for deployment
 __dirname = path.resolve();
-app.use(expres.static(path.join(__dirname, '/frontend/build')))
+app.use(express.static(path.join(__dirname, '/frontend/build')))
 app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 } )
