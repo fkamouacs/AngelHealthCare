504bc43d301ee1412f151e883b81634402908118
NotakingButaGod <214561763whe@gmail.com>
2024-04-04 20:52:01 -0400

    initialized the view account info modal in admin account page

--- frontend/src/components/admin_account_page.jsx
+++ frontend/src/components/admin_account_page.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Grid, Paper, Typography, IconButton, TextField, Button, List, ListItem, ListItemText, ListItemSecondaryAction, Box, Modal} from '@mui/material';
+import { Grid, Paper, Typography, IconButton, TextField, Button, List, ListItem, ListItemText, ListItemSecondaryAction, Box, Modal, Menu, MenuItem, ToggleButtonGroup, ToggleButton} from '@mui/material';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 // import MoreVertIcon from '@mui/icons-material/MoreVert.js';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
@@ -7,7 +7,13 @@ import CloseIcon from '@mui/icons-material/Close';
 
 export default function AdminAccountPage({PAGES, setPage}){
     const [openModal, setOpenModal] = useState(false);
-    
+    const [openAccountInfoModal, setOpenAccountInfoModal] = useState(false);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [accountState, setAccountState] = useState('Active');
+
+    const handleState = (event, newState) => {
+        setAccountState(newState);
+    };
     const handleModalOpen = () => {
         setOpenModal(true);
     };
@@ -19,6 +25,35 @@ export default function AdminAccountPage({PAGES, setPage}){
     const handLogOut = (event) => {
         setPage(PAGES.LOGIN);
     }
+
+    const handleMenuClick = (event) => {
+        setAnchorEl(event.currentTarget);
+        event.stopPropagation();
+    };
+    
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleAccountInfoModalOpen = () => {
+        setOpenAccountInfoModal(true);
+        handleMenuClose();
+    };
+      
+    const handleAccountInfoModalClose = () => {
+        setOpenAccountInfoModal(false);
+    };
+
+    const renderMenu = (
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+          elevation={5}
+        >
+          <MenuItem onClick={handleAccountInfoModalOpen} >Account Info</MenuItem>
+        </Menu>
+    );
 
     const renderModal = (
         <Modal
@@ -160,6 +195,186 @@ export default function AdminAccountPage({PAGES, setPage}){
             </Box>
         </Modal>
     );
+    
+    const renderAccountInfoModal = (
+        <Modal
+            open={openAccountInfoModal}
+            BackdropProps={{
+                style: { backgroundColor: 'rgba(0, 0, 0, 0.1)' } // Adjust the opacity here
+            }}
+            
+        >
+            <Box 
+                sx={{
+                    position: 'absolute', // Ensure it's fixed in the viewport
+                    top: '50%', // Center vertically
+                    left: '50%', // Center horizontally
+                    transform: 'translate(-50%, -50%)', // Adjust for exact centering
+                    width: 600,  // Fixed width
+                    height: 400, // Fixed height
+                    bgcolor: '#F0F8FF',
+                    p: 2, // Add some padding inside the box
+                    borderRadius: 2, // Optional: for rounded corners
+                    outline: 0 // Remove default focus outline
+                  }}
+            >
+                <Grid container marginBottom={2}>
+                    <Grid item xs={6} fontSize={40}>
+                        <Typography fontSize={'30px'} color={'#6682c4'} sx={{}}>
+                                Account Info
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6} display="flex" justifyContent="flex-end">
+                        <IconButton 
+                            onClick={handleAccountInfoModalClose}
+                        >
+                            <CloseIcon sx={{ borderRadius: '50px', borderColor: '#6682c4', borderWidth: '1', borderStyle: 'solid', color: '#6682c4'}}/>
+                        </IconButton>
+                    </Grid>
+                </Grid>
+                <Grid container >
+                    <Grid container item xs={5} >
+                        <Grid item container xs={12} py={2} alignContent={'center'}>
+                            <Typography fontWeight={'bold'} color={'#6682c4'}>
+                                Provider Name
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} py={2} alignContent={'center'}>
+                            <Typography fontWeight={'bold'} color={'#6682c4'}>
+                                Employee ID
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} py={2}>
+                            <Typography fontWeight={'bold'} color={'#6682c4'}>
+                                Contact Number
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} py={2}>
+                            <Typography fontWeight={'bold'} color={'#6682c4'}>
+                                Email
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid container item xs={6}>
+                        <Grid item container xs={12} alignContent={'center'}>
+                            <TextField
+                                label="Provider Name"
+                                inputProps={{
+                                    style: {
+                                        padding: 5,
+                                        backgroundColor:'white',
+                                    }
+                                }}
+                                InputLabelProps={{
+                                    shrink: true
+                                }}
+                                size="small"
+                                sx={{ width: '225px' }}
+                                value={"Paul fder"}
+                            />
+                        </Grid>
+                        <Grid item container xs={12} alignContent={'center'}>
+                            <TextField
+                                label="Employee Id"
+                                inputProps={{
+                                    style: {
+                                    padding: 5,
+                                    backgroundColor:'white',
+                                    }
+                                }}
+                                InputLabelProps={{
+                                    shrink: true
+                                }}
+                                sx={{ width: '225px' }}
+                                size="small"
+                                value={"4562313"}
+                            />
+                        </Grid>
+                        <Grid item container xs={12} alignContent={'center'}>
+                            <TextField
+                                label="Contact Number"
+                                inputProps={{
+                                    style: {
+                                    padding: 5,
+                                    backgroundColor:'white',
+                                    }
+                                }}
+                                InputLabelProps={{
+                                    shrink: true
+                                }}
+                                sx={{ width: '225px' }}
+                                size="small"
+                                value={"456413133"}
+                            />
+                        </Grid>
+                        <Grid item container xs={12} alignContent={'center'}>
+                            <TextField
+                                label="Email"
+                                inputProps={{
+                                    style: {
+                                    padding: 5,
+                                    backgroundColor:'white',
+                                    }
+                                }}
+                                InputLabelProps={{
+                                    shrink: true
+                                }}
+                                size="small"
+                                sx={{ width: '225px' }}
+                                value={"asd45as"}
+                            />
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid container marginBottom={2}>
+                    <Grid container item xs={5}>
+                        <Grid item xs={12} py={2}>
+                            <Typography fontWeight={'bold'} color={'#6682c4'}>
+                                Account Status
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid container item xs={6}>
+                        <Grid item container xs={12} alignContent={'center'}>
+                            <ToggleButtonGroup
+                                color="primary"
+                                value={accountState}
+                                exclusive
+                                onChange={handleState}
+                                aria-label="account state"
+                                sx={{ borderRadius: '50px', borderColor: '#6682c4', borderWidth: '1', borderStyle: 'solid'}}
+                                style={{ height: '50%' }}
+                            >
+                                <ToggleButton sx={{ borderRadius: '50px', borderColor: '#6682c4', borderWidth: '1', borderStyle: 'solid'}} value="Active">Active</ToggleButton>
+                                <ToggleButton sx={{ borderRadius: '50px', borderColor: '#6682c4', borderWidth: '1', borderStyle: 'solid'}} value="Disable">Disable</ToggleButton>
+                            </ToggleButtonGroup>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Box display="flex" marginTop={3} gap={1} justifyContent="center">
+                    <Button
+                        variant="contained"
+                        sx={{
+                            bgcolor: '#6682c4'
+                        }}
+                        onClick={()=>alert("Delete Account not implemented yet")}
+                        >
+                            Delete Account
+                    </Button>
+                    <Button
+                        variant="contained"
+                        sx={{
+                            bgcolor: '#6682c4'
+                        }}
+                        onClick={()=>alert("Save Account not implemented yet")}
+                        >
+                            Save Account
+                    </Button>
+                </Box>
+            </Box>
+        </Modal>
+    );
+
     const resources = [
         {_id: 1, name: 'Paul fder', status: "Active", email: "asd45as" },
         {_id: 2, name: 'Paul fder', status: "Active", email: "asd45as" },
@@ -211,6 +426,7 @@ export default function AdminAccountPage({PAGES, setPage}){
                             Create Account
                         </Button>
                         {renderModal}
+                        {renderAccountInfoModal}
                 </Grid>
                 <List dense={true}>
                     {resources.map((resource) => (
@@ -237,9 +453,16 @@ export default function AdminAccountPage({PAGES, setPage}){
                             </Grid>
                             <Grid item xs={1}>
                             <ListItemSecondaryAction>
-                                <IconButton edge="end" aria-label="details">
-                                <PlayArrowIcon sx={{color: '#6682c4'}}/>
+                                <IconButton 
+                                    edge="end" 
+                                    aria-label="more"
+                                    aria-controls="long-menu"
+                                    aria-haspopup="true"
+                                    onClick={handleMenuClick}
+                                >
+                                    <PlayArrowIcon sx={{color: '#6682c4'}} />
                                 </IconButton>
+                                {renderMenu}
                             </ListItemSecondaryAction>
                             </Grid>
                         </Grid>
