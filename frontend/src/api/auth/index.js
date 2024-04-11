51e125b5334cfce4c9f3b780c031630a956322da
fkamouacs <kamouaferid@gmail.com>
2024-04-11 17:11:39 -0400

    eslint

--- frontend/src/api/auth/index.js
+++ frontend/src/api/auth/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { createContext, useEffect, useState } from "react";
 //import { useHistory } from 'react-router-dom'
 import api from './auth.js'
