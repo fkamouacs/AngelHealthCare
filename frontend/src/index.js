141f0048b631cf64c3b67be42bf09404ffa1345f
fkamouacs <kamouaferid@gmail.com>
2024-04-12 16:58:48 -0400

    fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -21,11 +21,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
+import { AuthContextProvider } from './api/auth/index.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AuthContextProvider>
     <App />
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
