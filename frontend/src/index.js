01523d097e11c4d20110c40aa9e5f94df25641f6
fkamouacs <kamouaferid@gmail.com>
2024-04-12 17:03:04 -0400

    fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -21,7 +21,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
-import AuthContextProvider  from './api/auth/index.js';
+import { AuthContextProvider } from './api/auth/index.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
