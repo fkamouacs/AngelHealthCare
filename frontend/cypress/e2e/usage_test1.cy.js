43841f96ce8b3d218b1656319b76fe1c9fcd731f
Impeg <huifu.li@stonybrook.edu>
2024-04-21 21:00:56 -0400

    cypress run fix

--- frontend/cypress/e2e/usage_test1.cy.js
+++ frontend/cypress/e2e/usage_test1.cy.js
@@ -12,30 +12,30 @@ describe('usage test 1', () => {
 
     it('login test and navigate different tabs and add a resource item', () => {
         // Find any button that contains the text "LOG IN"
-        cy.get('button').contains('log in', {matchCase: false}).should('be.visible').trigger("click");
+        cy.get('button').contains('log in', {matchCase: false}).should('be.visible').trigger("click", {force: true});
 
         cy.get('input[type="email"]').should('be.visible').type("jr@gmail.com", { force: true });
         cy.get('input[type="password"]').should('be.visible').type("12345678", { force: true });
 
-        cy.get('button').contains('login', {matchCase: false}).should('be.visible').trigger("click");
+        cy.get('button').contains('login', {matchCase: false}).should('be.visible').trigger("click", {force: true});
 
-        cy.get('button').contains('rooms', {matchCase: false}).should('be.visible').trigger("click");
-        cy.get('button').contains('resources', {matchCase: false}).should('be.visible').trigger("click");
-        cy.get('button').contains('processes', {matchCase: false}).should('be.visible').trigger("click");
-        cy.get('button').contains('accounts', {matchCase: false}).should('be.visible').trigger("click");
-        cy.get('button').contains('view messages', {matchCase: false}).should('be.visible').trigger("click");
+        cy.get('button').contains('rooms', {matchCase: false}).should('be.visible').trigger("click", {force: true});
+        cy.get('button').contains('resources', {matchCase: false}).should('be.visible').trigger("click", {force: true});
+        cy.get('button').contains('processes', {matchCase: false}).should('be.visible').trigger("click", {force: true});
+        cy.get('button').contains('accounts', {matchCase: false}).should('be.visible').trigger("click", {force: true});
+        cy.get('button').contains('view messages', {matchCase: false}).should('be.visible').trigger("click", {force: true});
 
-        cy.get('button').contains('open drawer', {matchCase: false}).should('be.visible').trigger("click");
+        cy.get('button').contains('open drawer', {matchCase: false}).should('be.visible').trigger("click", {force: true});
         cy.get('.MuiDrawer-root > .MuiPaper-root').contains('Add New Resource', {matchCase: false}).should('be.visible');
-        cy.get('span').contains('Add New Resource', {matchCase: false}).should('be.visible').trigger("click");
+        cy.get('span').contains('Add New Resource', {matchCase: false}).should('be.visible').trigger("click", {force: true});
 
         const inputValues = ['mask', Math.floor(Math.random() * (100000 - 1000 + 1) + 1000), 'These are for staff memebers only!'];
         cy.get('input').each((element, index) => {
             // Use `cy.wrap()` to convert the yielded jQuery element back into a Cypress chainable object
             cy.wrap(element).type(inputValues[index], { force: true });
           });
-        cy.get('button').contains('add resource', {matchCase: false}).should('be.visible').trigger("click");
-        cy.get('button').contains('resources', {matchCase: false}).should('be.visible').trigger("click");
+        cy.get('button').contains('add resource', {matchCase: false}).should('be.visible').trigger("click", {force: true});
+        cy.get('button').contains('resources', {matchCase: false}).should('be.visible').trigger("click", {force: true});
         cy.contains(inputValues[0]).should('exist');
         cy.contains(inputValues[1]).should('exist');
     });
