48ef1c43857452071df8b6b60c8daae7ad009d75
Impeg <huifu.li@stonybrook.edu>
2024-04-26 17:30:47 -0400

    change to heroku website

--- frontend/cypress/e2e/usage_test1.cy.js
+++ frontend/cypress/e2e/usage_test1.cy.js
@@ -1,30 +1,28 @@
 
 describe('usage test 1', () => {
     beforeEach(() => {
-        cy.visit("http://localhost:3000");
-        cy.viewport(2560, 1440);
+        cy.visit('https://angelhealthcare-6befd2c18f64.herokuapp.com/');
     });
 
     const wait = () => cy.wait(1000);
 
     it('login test and navigate different tabs and add a resource item', () => {
 
-        cy.contains('Angel Health Care').should('be.visible');cy.wait(100);wait();
-        cy.get('button').contains('log in', {matchCase: false}).should('be.visible').click({ force: true });wait();
+        cy.get('button').contains('log in', {matchCase: false}).click({ force: true });wait();
 
-        cy.get('input[type="email"]').should('be.visible').type("huifu.li@stonybrook.edu", { force: true });wait();
-        cy.get('input[type="password"]').should('be.visible').type("12345678", { force: true });wait();
+        cy.get('input[type="email"]').type("huifu.li@stonybrook.edu", { force: true });wait();
+        cy.get('input[type="password"]').type("12345678", { force: true });wait();
 
-        cy.get('button').contains('login', {matchCase: false}).should('be.visible').click({ force: true });wait();
+        cy.get('button').contains('login', {matchCase: false}).click({ force: true });wait();
 
-        cy.get('button').contains('rooms', {matchCase: false}).should('be.visible').click({ force: true });wait();
-        cy.get('button').contains('resources', {matchCase: false}).should('be.visible').click({ force: true });wait();
-        cy.get('button').contains('processes', {matchCase: false}).should('be.visible').click({ force: true });wait();
-        cy.get('button').contains('account', {matchCase: false}).should('be.visible').click({ force: true });wait();
-        cy.get('button').contains('view messages', {matchCase: false}).should('be.visible').click({ force: true });wait();
+        cy.get('button').contains('rooms', {matchCase: false}).click({ force: true });wait();
+        cy.get('button').contains('resources', {matchCase: false}).click({ force: true });wait();
+        cy.get('button').contains('processes', {matchCase: false}).click({ force: true });wait();
+        cy.get('button').contains('account', {matchCase: false}).click({ force: true });wait();
+        cy.get('button').contains('view messages', {matchCase: false}).click({ force: true });wait();
 
-        cy.get('button').contains('admintoolbar', {matchCase: false}).should('be.visible').click({ force: true });wait();
-        cy.get('span').contains('add new resource', {matchCase: false}).should('be.visible').click({ force: true });wait();
+        cy.get('button').contains('admintoolbar', {matchCase: false}).click({ force: true });wait();
+        cy.get('span').contains('add new resource', {matchCase: false}).click({ force: true });wait();
         
         const inputValues = ['mask', Math.floor(Math.random() * (100000 - 1000 + 1) + 1000), 'These are for staff memebers only!'];
         cy.get('input').each((element, index) => {
@@ -32,8 +30,8 @@ describe('usage test 1', () => {
             cy.wrap(element).type(inputValues[index], { force: true });wait();
           });
           
-        cy.get('button').contains('add resource', {matchCase: false}).should('be.visible').click({ force: true });wait();
-        cy.get('button').contains('resources', {matchCase: false}).should('be.visible').click({ force: true });wait();
+        cy.get('button').contains('add resource', {matchCase: false}).click({ force: true });wait();
+        cy.get('button').contains('resources', {matchCase: false}).click({ force: true });wait();
         cy.contains(inputValues[0]).should('exist');
         cy.contains(inputValues[1]).should('exist');
 
--- frontend/cypress/e2e/usage_test2.cy.js
+++ frontend/cypress/e2e/usage_test2.cy.js
@@ -5,39 +5,17 @@ describe('usage test 1', () => {
         cy.viewport(2560, 1440);
     });
 
-    const findElementInFrame = (elementType, searchText) => {
-        return cy.get('iframe').then($iframes => {
-          // Iterate over each iframe
-          const iframeElements = [];
-          $iframes.each((index, iframe) => {
-            const $iframeBody = cy.wrap(iframe.contentDocument).find('body').should('not.be.undefined');
-            iframeElements.push({ $iframeBody, iframe });
-          });
-          // Wait for all iframe bodies to resolve
-          return Cypress.Promise.all(iframeElements.map(({ $iframeBody }) => $iframeBody)).then($iframeBodies => {
-            for (let i = 0; i < $iframeBodies.length; i++) {
-              const $iframeBody = $iframeBodies[i];
-              if ($iframeBody) {
-                const $element = $iframeBody.find(`${elementType}:contains("${searchText}")`, { matchCase: false });
-                if ($element.length > 0) {
-                  return { $element, iframe: iframeElements[i].iframe };
-                }
-              }
-            }
-            return null;
-          });
-        });
-      };
+    const wait = () => cy.wait(1000);
 
     it('login test and navigate different tabs and add a resource item', () => {
 
         cy.contains('Angel Health Care').should('be.visible');wait();
         cy.get('button').contains('log in', {matchCase: false}).should('be.visible').click({ force: true });wait();
 
-        cy.get('input[type="email"]').should('be.visible').type("huifu.li@stonybrook.edu", { force: true });wait();
-        cy.get('input[type="password"]').should('be.visible').type("12345678", { force: true });wait();
+        cy.get('input[type="email"]').should('be.visible').type("huifu.li@stonybrook.edu", { force: true });
+        cy.get('input[type="password"]').should('be.visible').type("12345678", { force: true });
 
-        cy.get('button').contains('login', {matchCase: false}).should('be.visible').click({ force: true });wait();
+        cy.get('button').contains('login', {matchCase: false}).should('be.visible').click({ force: true });
 
         cy.get('button').contains('account', {matchCase: false}).should('be.visible').click({ force: true });wait();
         cy.get('button').contains('view messages', {matchCase: false}).should('be.visible').click({ force: true });wait();
