d8b41e8862a28e0f29308440085377a36cb5c470
Impeg <huifu.li@stonybrook.edu>
2024-05-05 16:10:19 -0400

    socket fix

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,40 @@
-const http = require("http");
-
+const express = require('express');
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
-const uuid = require("uuid");
-const fs = require('fs');
-const readline = require('readline');
-const express = require('express');
-const request = require('supertest');
 const path = require('path');
 const socketIO = require("socket.io");
+const http = require("http");
+
 const app = express();
 
 mongoose.connect('mongodb+srv://feridkamoua:test@cluster0.tlfbqfv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// db.once('open', () => {
-//   console.log('Connected to MongoDB');
-// });
 
 const server = http.createServer(app);
 
 const io = socketIO(server, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-    },
-  });
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT"],
+  credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 io.on("connection", socket => {
     console.log("connected with client");
-    
+
     socket.on("notifyUpdate", () => io.emit("updated"));
     socket.on("logged in", () => {
         console.log("user logged in");
@@ -45,34 +46,13 @@ io.on("connection", socket => {
     socket.on("room updated", () => io.emit("room updated"));
     socket.on("patient updated", () => io.emit("patient updated"));
     socket.on("resource updated", () => io.emit("resource updated"));
-
-})
-
-server.listen(2456, () => {
-    console.log('Listening on *:2456');
 });
-
-// needed for deployment
-__dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
-  
-app.use(
-    cors({
-        origin: true,
-        methods: ["GET", "POST", "PUT"],
-        credentials: true
-    })
-);
-app.use(express.json());
-
-app.use(cookieParser());
 
 app.use((req, res, next) => {
-    console.log(`${req.method} request to ${req.path}`);
-    next();
+  console.log(`${req.method} request to ${req.path}`);
+  next();
 });
 
-// SETUP OUR OWN ROUTERS AS MIDDLEWARE
 const authRouter = require('./routes/auth-router')
 app.use('/auth', authRouter);
 
@@ -100,31 +80,26 @@ app.use('/api/email', emailRouter)
 const scheduleRouter = require('./routes/schedule-router')
 app.use('/api/schedule', scheduleRouter)
 
+const PORT = process.env.PORT || 3001;
 
-app.listen(process.env.PORT  || 3001, function(err){
-    if (err) console.log("Error in server setup")
-    console.log("Server listening on Port", 3001);
-})
 
 app.get("/api/", (req, res) => {
-    res.status(200).json({msg:"The get('/') was successful!"})
-})
+  res.status(200).json({msg:"The get('/') was successful!"})
+});
 
 app.get("/", (req, res) => {
-    res.status(200).json({msg:"The get('/1') was successful!"})
-})
+  res.status(200).json({msg:"The get('/1') was successful!"})
+});
 
-
-// deployment
 app.get('/*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-} )
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+});
 
 app.put('/*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-} )
-  
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+});
 
-module.exports = server;
-
-
+server.listen(PORT, function(err){
+  if (err) console.log("Error in server setup")
+  console.log("Server listening on Port", PORT);
+});
