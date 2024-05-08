78658dd2e07b081d8dbe496a2f9d6d51672b5421
NotakingButaGod <214561763whe@gmail.com>
2024-05-08 02:22:42 -0400

    test fix

--- frontend/cypress/e2e/login_to_account.cy.js
+++ frontend/cypress/e2e/login_to_account.cy.js
@@ -7,10 +7,10 @@ describe('LoginPage to Account Page Tests', () => {
     it('Homepage to login page to account page to resource page', () => {
         cy.get('button').contains('Log in').click();
         cy.get('input[type="email"]').type('wentao.he.1@stonybrook.edu');
-        cy.get('input[type="password"]').type('123456789');
+        cy.get('input[type="password"]').type('5201314whe');
   
         cy.get('input[type="email"]').should('have.value', 'wentao.he.1@stonybrook.edu');
-        cy.get('input[type="password"]').should('have.value', '123456789');
+        cy.get('input[type="password"]').should('have.value', '5201314whe');
         
         cy.get('button').contains('Login').click();
         cy.get('button').contains('Account').should('be.visible');
